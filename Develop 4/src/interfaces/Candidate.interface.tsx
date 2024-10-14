5d41cda7684287c4df9da1257a4c6e8680521101
Colby04 <cjcodes2024@gmail.com>
2024-10-14 13:48:09 -0500

    Installed react & vite, edited gitignore

--- Develop 4/src/interfaces/Candidate.interface.tsx
+++ Develop 4/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,13 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  experience: number;
+  education: string;
+  skills: string[];
+  status: string;
+  created_at: string;
+  updated_at: string;
+}
--- Develop 4/src/pages/CandidateSearch.tsx
+++ Develop 4/src/pages/CandidateSearch.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { searchGithub, searchGithubUser } from '../api/API';
+import { searchGithub, searchGithubUser } from './Develop/src/api/API.tsx';
 
 const CandidateSearch = () => {
   return <h1>CandidateSearch</h1>;
